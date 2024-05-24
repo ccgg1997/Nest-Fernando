350ab3f2b987c46ac3074713aaf3129a263a242f
Cristian Gómez <89625031+ccgg1997@users.noreply.github.com>
2024-05-24 16:43:54 +0000

    final de typescript

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import './style.css'
 import typescriptLogo from './typescript.svg'
 import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.ts'
-import {charmander} from './bases/05-decorators'
+import {charmander} from './bases/06-decorators2'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
